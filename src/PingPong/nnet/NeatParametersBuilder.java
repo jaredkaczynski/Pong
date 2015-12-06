79156bc6cc322e441012ac8a7acb4df91f41aac5
Jared <razrsword@gmail.com>
2015-12-06 11:31:27 -0500

    Progress with instantiation error

--- src/PingPong/nnet/NeatParametersBuilder.java
+++ src/PingPong/nnet/NeatParametersBuilder.java
@@ -1,14 +1,6 @@
 package PingPong.nnet;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.neuroph.contrib.neat.gen.Evolver;
 import org.neuroph.contrib.neat.gen.NeatParameters;
-import org.neuroph.contrib.neat.gen.NeuronGene;
-import org.neuroph.contrib.neat.gen.NeuronType;
-import org.neuroph.contrib.neat.gen.Organism;
 import org.neuroph.contrib.neat.gen.impl.SimpleNeatParameters;
 import org.neuroph.contrib.neat.gen.operations.MutationOperation;
 import org.neuroph.contrib.neat.gen.operations.ReproductionOperation;
@@ -18,28 +10,51 @@ import org.neuroph.contrib.neat.gen.operations.mutation.WeightMutationOperation;
 import org.neuroph.contrib.neat.gen.operations.reproduction.AbstractReproductionOperation;
 import org.neuroph.contrib.neat.gen.operations.selector.NaturalSelectionOrganismSelector;
 import org.neuroph.contrib.neat.gen.operations.speciator.DynamicThresholdSpeciator;
-import org.neuroph.contrib.neat.gen.persistence.PersistenceException;
 import org.neuroph.contrib.neat.gen.persistence.impl.DirectoryOutputPersistence;
 import org.neuroph.contrib.neat.gen.persistence.impl.serialize.JavaSerializationDelegate;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.*;
 
-public class EvolvePingPong {
-    public static final String OUTPUT_DIRECTORY = "evolve-pong";
-    public static final String BASE_DIRECTORY = "pong-output";
-    public static void main(String[] args) {
-        EvolvePingPong er = new EvolvePingPong();
-        er.evolve();
+/**
+ * Created by razrs on 06-Dec-15.
+ */
+public class NeatParametersBuilder {
+    public static final String BASE_DIRECTORY = "robot-output";
+
+    public static NeatParameters neatParameters;
+    public static long organismId;
+
+
+
+    private JButton worldChooserButton;
+    private JTextField organismIdField;
+    private boolean useOrganismId;
+
+    private NeatParametersBuilder(boolean useOrganismId) {
+        this.useOrganismId = useOrganismId;
+
     }
 
 
-    public EvolvePingPong() {
+    private long getOrganismId() {
+        return Long.valueOf(organismIdField.getText());
     }
 
-    /*public NeatParameters createNeatParameters() {
+    public static void getTrainParams() {
+        NeatParametersBuilder paramsPan = new NeatParametersBuilder(false);
+        neatParameters = paramsPan.createNeatParameters();
+    }
+
+    public NeatParameters createNeatParameters() {
         SimpleNeatParameters params = new SimpleNeatParameters();
         params.setMaximumGenerations(750);
         params.setPopulationSize(250);
-        //params.setMaximumFitness(0);
+        //params.setMaximumFitness(builder.getWorstFitnessScore());
         params.setFitnessFunction(new PingPongFitnessFunction());
 
         DynamicThresholdSpeciator speciator = new DynamicThresholdSpeciator();
@@ -50,7 +65,7 @@ public class EvolvePingPong {
                 .getOrganismSelector();
         selector.setKillUnproductiveSpecies(true);
 
-        List<MutationOperation> ops = new ArrayList<MutationOperation>();
+        java.util.List<MutationOperation> ops = new ArrayList<MutationOperation>();
 
         AddNeuronMutationOperation addNeuron = new AddNeuronMutationOperation(
                 0.5);
@@ -72,43 +87,7 @@ public class EvolvePingPong {
         // if you change this to a different persistence mechanism, make sure to change the value
         // in the replay as well.
         params.setPersistence(new DirectoryOutputPersistence(BASE_DIRECTORY, new JavaSerializationDelegate(false)));
-        NeatParameters test = params;
-        return test;
-    }*/
 
-    private void evolve() {
-        try {
-
-            NeatParametersBuilder.getTrainParams();
-            NeatParameters params = NeatParametersBuilder.neatParameters;
-            Evolver ev = Evolver.createNew(params, createInputLayer(params), createOutputLayer(params));
-            Organism o = ev.evolve();
-        } catch (PersistenceException e) {
-            throw new IllegalStateException("PersistenceException thrown evolving the robot behaviour.", e);
-        }
+        return params;
     }
-
-
-    public List<NeuronGene> createInputLayer(NeatParameters params) {
-        List<NeuronGene> inputs = new ArrayList<NeuronGene>();
-
-        for (int i = 0; i < 12; i++) {
-            NeuronGene ng = new NeuronGene(NeuronType.INPUT, params);
-            inputs.add(ng);
-        }
-
-        return inputs;
-    }
-
-    public List<NeuronGene> createOutputLayer(NeatParameters params) {
-        List<NeuronGene> outputs = new ArrayList<NeuronGene>();
-
-        for (int i = 0; i < 2; i++) {
-            NeuronGene ng = new NeuronGene(NeuronType.OUTPUT, params);
-            outputs.add(ng);
-        }
-
-        return outputs;
-    }
-
 }
